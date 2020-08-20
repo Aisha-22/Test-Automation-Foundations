d6e06f292e86b02c7bb114d3330aaa74a695259d
Aisha <ash.hlatshwayo22@gmail.com>
2020-08-21 00:09:24 +0200

    automation fundation

---
+++ models/cart.js
@@ -0,0 +1,46 @@
+module.exports = function Cart(oldCart) {
+    this.items = oldCart.items || {};
+    this.totalQty = oldCart.totalQty || 0;
+    this.totalPrice = oldCart.totalPrice || 0;
+
+    // Four funtions that discribe the behaviour of the Application
+    // 1. Taks in the "item" and "id" to be added to the cart
+    this.add = function(item, id) {
+        var storedItem = this.items[id];
+        if (!storedItem) {
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0};
+        }
+        storedItem.qty++;
+        storedItem.price = storedItem.item.price * storedItem.qty;
+        this.totalQty++;
+        this.totalPrice += storedItem.item.price;
+    };
+
+    // Takes in an "id" and reduces the quantity ofthe sticker by 1
+    this.reduceByOne = function(id) {
+        this.items[id].qty--;
+        this.items[id].price -= this.items[id].item.price;
+        this.totalQty--;
+        this.totalPrice -= this.items[id].item.price;
+
+        if (this.items[id].qty <= 0) {
+            delete this.items[id];
+        }
+    };
+
+    // Takes in an "id" and removes all quantity for a given sticker sticker
+    this.removeItem = function(id) {
+        this.totalQty -= this.items[id].qty;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
+    };
+    
+    // Creating an Arry on at the stickers in a cart
+    this.generateArray = function() {
+        var arr = [];
+        for (var id in this.items) {
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+};
